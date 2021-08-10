a3bb2269a90642404cb329a776066923928cf888
Tonmoy <tonmoy.cse.ctg@gmail.com>
2021-08-11 01:20:58 +0600

    urls, views and settings updated

--- protfolio/employee/views.py
+++ protfolio/employee/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 def profile(request):
     profile = "This is our employee profile page"
     return HttpResponse(profile)
+def employee(request):
+    profile = "This is our employee Home page"
+    return HttpResponse(profile)
 
--- protfolio/protfolio/views.py
+++ protfolio/protfolio/views.py
@@ -1,13 +1,13 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    home = "This is our home page"
-    return HttpResponse(home)
+    return render(request,'index.html')
 
 def about(request):
     about = "This is our about page"
     return HttpResponse(about)
 
 def contact(request):
-    contact = "This is our cntact page"
+    contact = "This is our contact page"
     return HttpResponse(contact)
