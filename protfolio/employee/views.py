d0108bb6f19abb14c1a86c32baeda78a24218da2
Tonmoy <tonmoy.cse.ctg@gmail.com>
2021-08-11 08:36:59 +0600

    employee views upadted with profile.html

--- protfolio/employee/views.py
+++ protfolio/employee/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def profile(request):
-    profile = "This is our employee profile page"
-    return HttpResponse(profile)
+    #profile = "This is our employee profile page"
+    #return HttpResponse(profile)
+    return render(request,'employee/profile.html')
 def employee(request):
     profile = "This is our employee Home page"
     return HttpResponse(profile)
